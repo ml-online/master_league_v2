08baa2f64111b9cf8b9b30617fee1aa41c291b29
lucasdomi <lucasdomi@globo.com>
2016-06-24 11:54:38 -0300

    criando rodadas.
    teste

---
+++ templates/rodadas.php
@@ -0,0 +1,87 @@
+<?php
+	session_start("login_ml");
+	include("conexao.php");
+?>
+<?php
+  if(!isset($_SESSION["psn"]) || !isset($_SESSION["senha"]))
+  {
+    header("Location: login.php");
+    exit; //encerra as funções da páginas
+  }
+  else
+  {
+    //echo "Você está logado.";
+	$psn = $_SESSION["psn"];
+	//echo $psn;
+	
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) 
+	{
+		// last request was more than 30 minutes ago
+		session_unset();     // unset $_SESSION variable for the run-time 
+		session_destroy();   // destroy session data in storage
+		header("Location: logout.php");
+		exit; //encerra as funções da página
+	}
+	
+	$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+  }
+?>
+<html lang="pt-BR">
+    <head>
+        <title>Master League - CR Galáticos</title>
+
+        <meta charset="utf-8"/>
+        <meta name="author" content="CR Galaticos">
+        <meta name="description" content="Organization of CR Galaticos Master League">
+
+        <link rel="stylesheet" type="text/css" href="../static/css/base.css">
+        <link rel="stylesheet" type="text/css" href="../static/css/teste_tabela_css.css">
+		<link rel="stylesheet" href="../static/css/estilo2.css"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+        
+        <script src="auto-complete.js"></script>
+		
+			
+    </head>
+
+    <body>
+      <?php
+      	include("cabecalho.php");
+      ?>
+
+      <section class="present">
+          <h1 class="present__title">CR Galaticos - Master League</h1>
+      </section>
+        <section class="main-content">
+			<center>
+				<div style="padding-top:20px;">
+                                    Rodadas do campeonato
+                                    
+                                    <?php
+                                    $sql = " SELECT nomeEquipeCasa, nomeEquipeFora from rodadas";
+                                    $query = mysqli_query($con,$sql) or trigger_error("Query Failed! SQL: $query - Error: ". mysqli_error($con), E_USER_ERROR);
+							$rowcount = mysqli_num_rows($query);
+                                    ?><br /><?php
+                                    ?><br /><?php
+                                    ?>
+                                    Rodada 1
+                                    <?php
+                                    
+                                    // pegar o sql de cima, e colocar where rodada=1
+                                    
+                                    while($row=mysqli_fetch_array($query,MYSQLI_ASSOC))
+							{
+                                                                ?><br /><?php
+                                                                ?><br /><?php
+								echo $row["nomeEquipeCasa"] ;
+                                                                echo " x ";
+                                                                echo $row["nomeEquipeFora"];
+							}        
+                                    ?>
+                                    </center>
+        </section>
+         <?php
+      	include("footer.php");
+      ?>
+	</body>
+</html>
